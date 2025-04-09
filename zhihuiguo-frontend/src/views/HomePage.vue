06ebdb0ddae8853c4963689a4760ffea823f7785
Wcy <Wcy@666.com>
2025-04-09 16:53:35 +0800

    代码调整

--- zhihuiguo-frontend/src/views/HomePage.vue
+++ zhihuiguo-frontend/src/views/HomePage.vue
@@ -2,39 +2,39 @@
   <div id="homePage">
     <div class="searchBar">
       <a-input-search
-        :style="{ width: '320px' }"
-        placeholder="快速发现答题应用"
-        button-text="搜索"
-        size="large"
-        search-button
+          :style="{ width: '320px' }"
+          placeholder="快速发现答题应用"
+          button-text="搜索"
+          size="large"
+          search-button
       />
     </div>
     <a-list
-      class="list-demo-action-layout"
-      :grid-props="{ gutter: [20, 20], sm: 24, md: 12, lg: 8, xl: 6 }"
-      :bordered="false"
-      :data="dataList"
-      :pagination-props="{
+        class="list-demo-action-layout"
+        :grid-props="{ gutter: [20, 20], sm: 24, md: 12, lg: 8, xl: 6 }"
+        :bordered="false"
+        :data="dataList"
+        :pagination-props="{
         pageSize: searchParams.pageSize,
         current: searchParams.current,
         total,
       }"
-      @page-change="onPageChange"
+        @page-change="onPageChange"
     >
       <template #item="{ item }">
-        <AppCard :app="item" />
+        <AppCard :app="item"/>
       </template>
     </a-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import {ref, watchEffect} from "vue";
 import AppCard from "@/components/AppCard.vue";
 import API from "@/api";
-import { listAppVoByPageUsingPost } from "@/api/appController";
+import {listAppVoByPageUsingPost} from "@/api/appController";
 import message from "@arco-design/web-vue/es/message";
-import { REVIEW_STATUS_ENUM } from "@/constant/app";
+import {REVIEW_STATUS_ENUM} from "@/constant/app";
 
 // 初始化搜索条件（不应该被修改）
 const initSearchParams = {
